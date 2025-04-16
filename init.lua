79631a39c7d1e1b3f01865facc33f02dbef9063a
MedArf <arfa.mehdi@gmail.com>
2025-04-16 16:21:50 +0200

    cleaning

--- init.lua
+++
@@ -1,6 +0,0 @@
-require("config.autocmds")
-require("config.keymaps")
-require("config.lazy")
-
-
-
--- lua/plugins/nvim-tmux-navigator.lua
+++
@@ -1,9 +0,0 @@
-return{
-  "christoomey/vim-tmux-navigator",
-  vim.keymap.set("n", "<c-h>", "<cmd>TmuxNavigateLeft<cr>", { noremap = true, silent = true }),
-  vim.keymap.set("n", "<c-j>", "<cmd>TmuxNavigateDown<cr>", { noremap = true, silent = true }),
-  vim.keymap.set("n", "<c-k>", "<cmd>TmuxNavigateUp<cr>", { noremap = true, silent = true }),
-  vim.keymap.set("n", "<c-l>", "<cmd>TmuxNavigateRight<cr>", { noremap = true, silent = true }),
-}
-
-
